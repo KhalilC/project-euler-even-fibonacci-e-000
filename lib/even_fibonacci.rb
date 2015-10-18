b7ed4c951cedfeec05e3e7d8d91e0bf426d0b42b
KhalilC <klcrumpler@gmail.com>
2015-10-18 22:55:54 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+
+first_fibs = [1, 2]
+100.times { first_fibs << (first_fibs[-1] + first_fibs[-2])}
+def even_fibonacci_sum(limit)
+  first_fibs = [1, 2]
+  100.times { first_fibs << (first_fibs[-1] + first_fibs[-2])}
+  result = first_fibs.select { |number| number.even? && number < limit }.inject(:+)
+  result
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    first_fibs = [1, 2]
+    100.times { first_fibs << (first_fibs[-1] + first_fibs[-2])}
+    first_fibs.select { |number| number.even? && number < limit }.inject(:+)
+  end
+
+end
